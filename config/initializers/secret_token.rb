461d8e9f13e36c9142fbebccb33730b637c017c0
Ken Ledbetter <ken.ledbetter@gmail.com>
2013-02-01 20:02:07 -0600

    Added User and Session.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandRater::Application.config.secret_token = '65039ad8b9823f82df33b259352c3d973c0b2ea8bf5a26e873d178158bfd5fbf7c190a60ef7895957ce7cfb65f5e0098000176ede4d569d5576f7decb88e47ad'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+BandRater::Application.routes.draw do
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  resources :users
+  resources :sessions
+  root :to => 'users#index'
+end

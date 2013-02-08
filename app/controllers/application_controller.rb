3f58abfa061b90ac57b94fa1b3c6037ea3adaf67
Ken Ledbetter <ken.ledbetter@gmail.com>
2013-02-08 08:29:03 -0600

    Permission check and more festival infra.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,16 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def check_permission(options = {})
+    if current_user && current_user.admin
+      return true
+    elsif !options[:user].nil? && current_user == options[:user] && !options[:admin]
+      return true
+    else
+      return false
+    end
+  end
+
   helper_method :current_user
+  helper_method :check_permission
 end
--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,4 +1,4 @@
 class Festival < ActiveRecord::Base
   has_and_belongs_to_many :bands
-  attr_accessible :name
+  attr_accessible :name, :date
 end

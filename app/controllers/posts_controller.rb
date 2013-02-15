9fd784d67868f7588642ba6efbe234d322647986
Ken Ledbetter <ken.ledbetter@gmail.com>
2013-02-15 09:48:45 -0600

    More work on post controller and views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def new
     # Only allowed logged in reviewers to create posts
     if current_user && current_user.is_reviewer?
-      @post = Post.new
+      @post = Post.new(:user_id => current_user.id)
     else
       redirect_to posts_path, :notice => "Must be a reviewer to create a post"
     end
@@ -24,45 +24,50 @@ class PostsController < ApplicationController
   end
 
   def edit
-    # Only allowed logged in reviewers to modify posts
-    if current_user && current_user.is_reviewer?
-      if @post = Post.find_by_id(params[:id])
+    if @post = Post.find_by_id(params[:id])
+      # Only allow a logged in user or admin to modify a post
+      if check_permission(@post.user)
         render "edit"
       else
-        redirect_to @post, :notice => "Post does not exist"
+        redirect_to @post, :notice => "Cannot edit another user's post"
       end
     else
-      redirect_to posts_path, :notice => "Must be a reviewer to modify a post"
+      redirect_to posts_path, :notice => "Post does not exist"
     end
   end
 
   def update
-    # Only allowed logged in reviewers to modify posts
-    if current_user && current_user.is_reviewer?
-      if @post = Post.find_by_id(params[:id])
+    if @post = Post.find_by_id(params[:id])
+      # Only allow a logged in user or admin to modify a post
+      if check_permission(@post.user)
         if @post.update_attributes(params[:post])
           redirect_to @post, :notice => "Post updated"
         else
           render "edit"
         end
       else
-        redirect_to @post, :notice => "Post does not exist"
+        redirect_to @post, :notice => "Cannot edit another user's post"
       end
     else
-      redirect_to posts_path, :notice => "Must be a reviewer to modify a post"
+      redirect_to posts_path, :notice => "Post does not exist"
     end
   end
 
   def index
-    @posts = Post.order("average_rating desc").find(:all)
+    if current_user && current_user.is_reviewer?
+      # Show all posts
+      @posts = Post.includes(:user, :festival).order("created_at desc").limit(5)
+    else
+      # Show only published posts
+      @posts = Post.includes(:user, :festival).where(:publish => true).order("created_at desc").limit(5)
+    end
   end
 
   def show
-    # Get post and prefetch ratings
-    if @post = Post.find_by_id(params[:id])
+    if @post = Post.includes(:user, :festival).where(:id => params[:id], :publish => true).first
       render "show"
     else
-      redirect_to posts_path, :notice => "Must be a reviewer to create a post"
+      redirect_to posts_path, :notice => "Post does not exist"
     end
   end
 end
